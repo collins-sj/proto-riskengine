8df5c4f3533a49ef2ed9b02ed25e4d1fadf0916a
Stephen Collins <stephen.collins@paysafe.com>
2018-06-25 08:31:13 +0100

    Removed Test Class

--- src/main/java/scollins/blockchain/prototype/risk/riskengine/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package scollins.blockchain.prototype.risk.riskengine;
-import static spark.Spark.*;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-    }
-}
